11d25375199b659375591a9128fb3a220f4e753e
ndoss <nathan.doss@gmail.com>
2012-04-28 19:14:56 -0400

    Remove arg for frame rate, doesn't work right now

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ application:setBackgroundColor(0x000000)
 
 spriter = Spriter.new("BetaFormatHero.SCML", "example_hero.txt", "example_hero.png")
 
-a = spriter:movieClip("walk", true, 100)
+a = spriter:movieClip("walk", true)
 stage:addChild(a)
 
 b = spriter:movieClip("idle_healthy", true)
