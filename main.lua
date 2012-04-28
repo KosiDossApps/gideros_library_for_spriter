65cf2673ee99608fa54e5c040308229a278ed67e
ndoss <nathan.doss@gmail.com>
2012-04-28 18:07:34 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+-- ===================================================================================
+-- Copyright - KosiDoss Apps (http://kosidossapps.com)
+-- License - http://creativecommons.org/publicdomain/zero/1.0/
+-- ===================================================================================
+
+application:setBackgroundColor(0x000000)
+
+spriter = Spriter.new("BetaFormatHero.SCML", "example_hero.txt", "example_hero.png")
+
+a = spriter:movieClip("walk", true, 100)
+stage:addChild(a)
+
+b = spriter:movieClip("idle_healthy", true)
+stage:addChild(b)
+b:setPosition(0,250)
